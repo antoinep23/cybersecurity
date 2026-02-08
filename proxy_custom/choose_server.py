9afee6b0f7c5cf1d2ea42a8e495c8c3f9fbee0f4
Antoine <141731510+antoinep23@users.noreply.github.com>
2026-02-08 21:53:45 +0000

    implement auto-healing if proxied server's health-check fail

--- proxy_custom/choose_server.py
+++ proxy_custom/choose_server.py
@@ -1,5 +1,6 @@
 import asyncio
 import uuid
+import os
 
 servers = {
     'server1': {
@@ -22,6 +23,17 @@ servers = {
     }
 }
 
+async def get_healthy_servers_list():
+    tasks = [test_server(server) for server in servers.values()]
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    
+    healthy_servers = [server for server, result in zip(servers.values(), results) if result is True]
+
+    return healthy_servers
+
+def get_run_server_command(port): 
+    return f"python3 -m http.server {port}"
+
 async def test_server(server):
     try:
         _, writer = await asyncio.wait_for(
@@ -35,10 +47,16 @@ async def test_server(server):
         return False
 
 async def choose_server():
-    tasks = [test_server(server) for server in servers.values()]
-    results = await asyncio.gather(*tasks, return_exceptions=True)
-    
-    healthy_servers = [server for server, result in zip(servers.values(), results) if result is True]
+    healthy_servers = await get_healthy_servers_list()
+
+    if not healthy_servers:
+        for server in servers.values():
+            command = get_run_server_command(server['port'])
+            asyncio.create_task(asyncio.to_thread(lambda: os.system(command)))
+            await asyncio.sleep(2)
+
+        healthy_servers = await get_healthy_servers_list()
+
    
     if healthy_servers:
         chosen_server = min(healthy_servers, key=lambda s: s['last_hit'])
